2103000d27e7d0bc7ab4ec8e7686f62fee3922ba
Danilo Abreu <danilo13abreu@gmail.com>
2026-01-11 09:40:26 -0300

    Modificações no validador

--- src/actions/signup.action.ts
+++ src/actions/signup.action.ts
@@ -7,28 +7,34 @@ import { users } from "@/database/schemas/users.schema";
 import { hashPassword } from "@/lib/auth";
 import { eq } from "drizzle-orm";
 
-const action = createSafeActionClient();
+export async function signupAction(formData: FormData) {
+  const data = Object.fromEntries(formData);
 
-export const signupAction = action(
-  signupSchema,
-  async ({ name, email, password }) => {
-    const existingUser = await db
-      .select()
-      .from(users)
-      .where(eq(users.email, email));
-
-    if (existingUser.length > 0) {
-      throw new Error("Usuário já existe");
-    }
-
-    const passwordHash = await hashPassword(password);
-
-    await db.insert(users).values({
-      name,
-      email,
-      passwordHash,
-    });
-
-    return { success: true };
+  const parsed = signupSchema.safeParse(data);
+  if (!parsed.success) {
+    return {
+      error: parsed.error.flatten().fieldErrors,
+    };
   }
-);
+
+  const { name, email, password } = parsed.data;
+
+  const existingUser = await db
+    .select()
+    .from(users)
+    .where(eq(users.email, email));
+
+  if (existingUser.length > 0) {
+    return { error: { email: ["Usuário já existe"] } };
+  }
+
+  const passwordHash = await hashPassword(password);
+
+  await db.insert(users).values({
+    name,
+    email,
+    passwordHash,
+  });
+
+  return { success: true };
+}
